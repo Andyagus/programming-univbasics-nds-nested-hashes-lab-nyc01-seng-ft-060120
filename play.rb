21ff9d7ad859b6d60032907d5bd9fad69c973fb9
Andy Agus <redagus@gmail.com>
2020-05-22 21:49:28 +0000

    Automatically backed up by Learn

--- play.rb
+++ play.rb
@@ -1,19 +1,22 @@
-director_1 = {
-  name: "ida lubinovitch",
-  favorite_movie: "Great Gatsby"
-}
+director_1 = 
 
-director_2 = {
-  name: "Johnny chase"
-  favorite_movie: "Entourage"
-}
+director_2 = 
 
 director_3 = {
   name: "Arnold Luvtz!"
   favorite_movie: "A day past Shintzgerton"
 }
 
-film_fest = {
+film_fest = [
+{
+  name: "ida lubinovitch",
+  favorite_movie: "Great Gatsby"
+},
 
-
+{
+  name: "Johnny chase"
+  favorite_movie: "Entourage"
 }
+
+
+]
