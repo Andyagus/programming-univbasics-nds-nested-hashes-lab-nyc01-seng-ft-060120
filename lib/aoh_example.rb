da1c019ebaafba7cf4f15984ac13163deca7cc8d
Andy Agus <redagus@gmail.com>
2020-05-22 21:52:19 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -2,15 +2,13 @@
 
 DON_G = 
 JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-PAT_M =  
+PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
  assembly = [
   { name:  "Don Gately", occupation: "Live-in Staff" },
-  { name:  "Pat Monteseian", occupation: "Staff" },
-  
   
 ]
 end
